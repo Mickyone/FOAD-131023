b28bb7c78e127c59895d44cd17f0f33836d43797
Mickyone <micky-one^@outlook.com>
2023-10-13 16:54:47 +0200

    first commit

--- function.php
+++ function.php
@@ -1,18 +1,30 @@
 <?php
 
-
-$json = file_get_contents('https://dummyjson.com/user');
-$data = json_decode($json, true);
-dbug ($data);
-
-foreach ($data as $user) {
-    echo '<img src="' . $user['image'] . '" alt="Image de ' . $user['name'] . '">';
-
-    echo '<h4>Prénom : ' . $user['firstname'] . '</h4>';
-
-    echo '<p>Nom : ' . $user['lastname'] . '</p>';
-
-    echo '<p>Email : ' . $user['email'] . '</p>';
+function dbug($value){
+    echo '<pre style="background-color:brown;color:yellow;overflow:auto;padding:10px">';
+    print_r($value);
+    // var_dump($value);
+    echo '</pre>';
 }
 
+function dd($value){
+    dbug($value);
+    die('Script php arrêté !!!');
+}
+
+$json = file_get_contents('https://dummyjson.com/user');
+    
+$data = json_decode($json, true);
+
+// dd($data);
+
+$image = [];
+
+foreach ($data as $user) {
+    echo '<img src="' . $user['image'] . '">';
+    echo '<h4>Prénom : ' . $user['firstname'] . '</h4>';
+    echo '<p>Nom : ' . $user['lastname'] . '</p>';
+    echo '<p>Email : ' . $user['email'] . '</p>';
+}
+    
 ?>
