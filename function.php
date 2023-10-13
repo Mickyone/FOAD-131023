4c404d6827932a418db50f0d544cdd103e36e6fa
Mickyone <micky-one^@outlook.com>
2023-10-13 16:30:13 +0200

    maj

--- function.php
+++ function.php
@@ -1,23 +1,18 @@
-<?php 
+<?php
 
-function dbug($value){
-    echo '<pre style="background-color:brown;color:yellow;overflow:auto;padding:10px">';
-    print_r($value);
-    // var_dump($value);
-    echo '</pre>';
+
+$json = file_get_contents('https://dummyjson.com/user');
+$data = json_decode($json, true);
+dbug ($data);
+
+foreach ($data as $user) {
+    echo '<img src="' . $user['image'] . '" alt="Image de ' . $user['name'] . '">';
+
+    echo '<h4>Prénom : ' . $user['firstname'] . '</h4>';
+
+    echo '<p>Nom : ' . $user['lastname'] . '</p>';
+
+    echo '<p>Email : ' . $user['email'] . '</p>';
 }
-
-function dd($value){
-    dbug($value);
-    die('Script php arrêté !!!');
-}
-
-$colors = ['red', 'green', 'blue', 'cyan', 'magenta', 'yellow', 'black'];
-
-
-foreach($colors as $color){
-    echo ucwords($color) . '<br>';
-}
-
 
 ?>
